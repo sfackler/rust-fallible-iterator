926828693450e9abed300637c6d25a340b58fa3f
Steven Fackler <sfackler@palantir.com>
2016-05-04 13:31:05 -0700

    Add DoubleEndedFallibleIterator and rev

--- src/lib.rs
+++ src/lib.rs
@@ -31,6 +31,14 @@ impl<I: FallibleIterator + ?Sized> FallibleIterator for Box<I> {
     }
 }
 
+pub trait DoubleEndedFallibleIterator: FallibleIterator {
+    fn next_back(&mut self) -> Result<Option<Self::Item>, Self::Error>;
+
+    fn rev(self) -> Rev<Self> where Self: Sized {
+        Rev(self)
+    }
+}
+
 pub fn convert<T, E, I>(it: I) -> Convert<I> where I: Iterator<Item = Result<T, E>> {
     Convert(it)
 }
@@ -51,5 +59,30 @@ impl<T, E, I: Iterator<Item = Result<T, E>>> FallibleIterator for Convert<I> {
 
     fn size_hint(&self) -> (usize, Option<usize>) {
         self.0.size_hint()
+    }
+}
+
+pub struct Rev<I>(I);
+
+impl<I> FallibleIterator for Rev<I> where I: DoubleEndedFallibleIterator {
+    type Item = I::Item;
+    type Error = I::Error;
+
+    fn next(&mut self) -> Result<Option<I::Item>, I::Error> {
+        self.0.next_back()
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.0.size_hint()
+    }
+
+    fn count(self) -> Result<usize, I::Error> {
+        self.0.count()
+    }
+}
+
+impl<I> DoubleEndedFallibleIterator for Rev<I> where I: DoubleEndedFallibleIterator {
+    fn next_back(&mut self) -> Result<Option<I::Item>, I::Error> {
+        self.0.next()
     }
 }
