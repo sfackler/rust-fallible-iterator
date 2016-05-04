41498e6a2cd0877bd617619ece334590834b2687
Steven Fackler <sfackler@palantir.com>
2016-05-04 09:44:16 -0700

    Minimal implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,27 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
+pub trait FallibleIterator {
+    type Item;
+    type Error;
+
+    fn next(&mut self) -> Result<Option<Self::Item>, Self::Error>;
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (0, None)
+    }
+}
+
+impl<T, E, I: Iterator<Item = Result<T, E>>> FallibleIterator for I {
+    type Item = T;
+    type Error = E;
+
+    fn next(&mut self) -> Result<Option<T>, E> {
+        match Iterator::next(self) {
+            Some(Ok(i)) => Ok(Some(i)),
+            Some(Err(e)) => Err(e),
+            None => Ok(None),
+        }
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        Iterator::size_hint(self)
     }
 }
