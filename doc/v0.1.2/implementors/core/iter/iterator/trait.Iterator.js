2616238473c799aabdece921caeea2be2d01765e
Steven Fackler <sfackler@palantir.com>
2016-07-20 23:43:51 +0200

    Release v0.1.2

---
+++ doc/v0.1.2/implementors/core/iter/iterator/trait.Iterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["fallible_iterator"] = ["impl&lt;I&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/iterator/trait.Iterator.html' title='core::iter::iterator::Iterator'>Iterator</a> for <a class='struct' href='fallible_iterator/struct.Iterator.html' title='fallible_iterator::Iterator'>Iterator</a>&lt;I&gt; <span class='where'>where I: <a class='trait' href='fallible_iterator/trait.FallibleIterator.html' title='fallible_iterator::FallibleIterator'>FallibleIterator</a></span>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
