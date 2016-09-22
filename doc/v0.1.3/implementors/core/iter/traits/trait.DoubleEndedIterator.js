0e4d2c6d16416ef78dd8ddb8695a94f6fe4c8abe
Steven Fackler <sfackler@gmail.com>
2016-09-21 21:31:32 -0700

    Release v0.1.3

---
+++ doc/v0.1.3/implementors/core/iter/traits/trait.DoubleEndedIterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["fallible_iterator"] = ["impl&lt;I&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/traits/trait.DoubleEndedIterator.html' title='core::iter::traits::DoubleEndedIterator'>DoubleEndedIterator</a> for <a class='struct' href='fallible_iterator/struct.Iterator.html' title='fallible_iterator::Iterator'>Iterator</a>&lt;I&gt; <span class='where'>where I: <a class='trait' href='fallible_iterator/trait.DoubleEndedFallibleIterator.html' title='fallible_iterator::DoubleEndedFallibleIterator'>DoubleEndedFallibleIterator</a></span>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
